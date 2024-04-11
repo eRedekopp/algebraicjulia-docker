3ec3b592a0828550058dac35207a9b8cc4e89c3a
Eric Redekopp <eric.redekopp@usask.ca>
2024-04-10 18:59:15 -0600

    Initial

---
+++ rewriting/AddPackages.jl
@@ -0,0 +1,2 @@
+using Pkg
+Pkg.add.(["Catlab", "StructEquality", "AlgebraicPetri", "PrettyTables", "LabelledArrays", "Luxor", "OrdinaryDiffEq", "Plots", "DataMigrations", "AlgebraicRewriting", "IJulia", "GraphViz", "Graphs"])
---
+++ stockflow/AddPackages.jl
@@ -0,0 +1,2 @@
+using Pkg
+Pkg.add.(["Catlab", "AlgebraicRewriting", "LabelledArrays", "Luxor", "OrdinaryDiffEq", "Plots", "StockFlow", "IJulia", "GraphViz", "Graphs"])
